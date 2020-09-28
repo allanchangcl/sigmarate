e7d338f08eed4d77a3c77e293494062e36bc10dc
Allan Chang <allan@clchang.net>
2020-09-28 14:09:17 +0800

    Refactor, Remove unused routes

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var path = require("path")
 var cookieParser = require("cookie-parser")
 var logger = require("morgan")
 
-// var indexRouter = require("./routes/index")
-
 var app = express()
 app.use(helmet())
 
@@ -19,14 +17,12 @@ app.use(logger("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, "public")))
 
 app.use(function (req, res, next) {
   res.setHeader("Content-Security-Policy", "script-src 'self'")
   return next()
 })
 
-// app.use("/", indexRouter)
 app.use("/", express.static(path.join(__dirname, "./reactjs/build")))
 
 // catch 404 and forward to error handler
